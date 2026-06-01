df77fb8bcab3f8e63f1a65542e9d615daa2f8c09
heropaulexy <okeoluwaseun79@gmail.com>
2026-06-01 11:07:14 +0100

    fiixing email

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -5,7 +5,9 @@ const smtpPassword = process.env.SMTP_PASSWORD;
 const fromName = process.env.SMTP_FROM_NAME || "NexLeadership Community";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // use SSL
   auth: {
     user: smtpEmail,
     pass: smtpPassword,
